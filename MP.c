3b84421ff58cf515dad8912bf722bf6d7266f155
dfbdizon <diana.dizon22@gmail.com>
2016-03-23 21:08:38 +0800

    Added structs
    Coordinates, Car, GridState, Node

--- MP.c
+++ MP.c
@@ -1,26 +1,49 @@
 #include <stdio.h>
 #include <string.h>
+
+typedef struct{
+  int x;
+  int y;
+} Coordinates;
+
+typedef struct{
+  char id[1];
+  char orientation[1];
+  Coordinates *coor;
+} Car;
+
+typedef struct{
+  char **grid;
+} GridState;
+
+typedef struct Node{
+  int currentHeuristic = 0;
+  int currentCost = 0;
+  GridState *grid;
+  Car *cars;
+  Node **children;
+} Node;
+
 void getFile(FILE *fp, int *matrix){
 
   int ch = getc(fp);
   char c;
-  ch=ch-48;
+  ch = ch - 48;
   *matrix = ch;
-  do{
-
+  do {
     ch = getc(fp);
     
     /// for new lines
-    if (ch==10){
+    if (ch == 10){
         ch = getc(fp);
         printf("\n");
-
     }
-    /// for spaces ==32, for EOF <-1
-    if (ch!=32 && ch>0){ 
-        ch=ch-48;
+
+    /// for spaces == 32, for EOF <-1
+    if (ch != 32 && ch > 0){ 
+        ch = ch - 48;
         printf(" %d  ", ch );
-    } 
+    }    
   } while (ch != EOF); 
    
   if (feof(fp))
@@ -37,8 +60,11 @@ int main () {
     int matrix;
 
     fp = fopen("a.txt", "r");
-    getFile(fp, &matrix);
-    printf("\nmatrix limit %d\n", matrix);
+    /*getFile(fp, &matrix);
+    printf("\nmatrix limit %d\n", matrix);*/
+
+    // Initialize Grid // 
+
     fclose(fp);
     return 0;
 }
